29664426c9c99a3629241d6b7ab9b0431484a3dc
LDenninger <luis0512@web.de>
2023-04-22 13:42:24 +0200

    init file

---
+++ init.bash
@@ -0,0 +1,12 @@
+## RUN AFTER FIRST CLONING ##
+
+# Initialize the submodule containing the logger and experiment manager
+git submodule init
+git submodule update
+cd experiments
+git checkout master
+git pull
+
+# Aliases to initialize conda environments for different machines
+# CUDA 11
+alias init_c11='conda create -n vision_lab python=3.9 jupyter yaml ipdb scipy matplotlib tensorboard tqdm pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia -c conda-forge; conda activate vision_lab;'
